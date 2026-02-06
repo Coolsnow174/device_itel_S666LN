2e7cb2700d4c7615527714868d75d974b96ad279
Mashopy <eliasgheeraert@gmail.com>
2026-02-06 17:02:41 +0000

    S666LN: Set the correct parameter for A2DP suspend
    Why even renaming it in the first place ???
    
    Change-Id: I09d53ea1fd6fbca22aae6bcd48a627312b55cf6d
    
    Signed-off-by: KimelaZX <mmikailrei@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -52,7 +52,9 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('libsensorndkbridge.so', 'android.hardware.sensors@1.0-convert-shared.so'),
     'vendor/lib64/hw/audio.primary.mediatek.so': blob_fixup()
         .replace_needed('libalsautils.so', 'libalsautils-v31.so')
-        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so'),
+        .replace_needed('libtinyxml2.so', 'libtinyxml2-v34.so')
+        .binary_regex_replace(b'A2dpsuspendonly', b'A2dpSuspended\x00\x00')
+        .binary_regex_replace(b'BTAudiosuspend', b'A2dpSuspended\x00'),
     (
         'vendor/lib64/hw/sensors.mediatek.V2.0.so',
         'vendor/lib64/libcodec2_mtk_c2store.so',
