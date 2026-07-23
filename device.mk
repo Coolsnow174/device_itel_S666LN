1e493c0f46072a8231905e1220fb3df2c30339e7
Mashopy <eliasgheeraert@gmail.com>
2026-07-23 17:34:46 +0800

    S666LN: Use pre-U QPR2 WiFi HAL struct layout
    Change-Id: Ie4287a066ee1fc0c1f9ac4fdedd0aec7c5e8ba2e
    Signed-off-by: Shirayuki39 <lorddemecrius83@proton.me>
    Signed-off-by: KimelaZX <mmikailrei@gmail.com>
    Signed-off-by: putraaxzy <ptraasw17@gmail.com>

--- device.mk
+++ device.mk
@@ -516,6 +516,7 @@ PRODUCT_PACKAGES += \
    vndservice
     
 # Wi-Fi
+$(call soong_config_set_bool,mediatek_wifi_hal,use_pre_u_qpr2_struct,true)
 PRODUCT_PACKAGES += \
     libwifi-hal-wrapper \
     android.hardware.wifi-service \
